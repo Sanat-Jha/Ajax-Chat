bfacbcae5ba27c206a14facb5349c2365e268cfa
Hacker-SJ-2 <91521958+Hacker-SJ-2@users.noreply.github.com>
2021-10-01 12:07:44 +0530

    Update urls.py

--- AjaxChat/urls.py
+++ AjaxChat/urls.py
@@ -1,5 +1,4 @@
 """AjaxChat URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -15,7 +14,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls'))
